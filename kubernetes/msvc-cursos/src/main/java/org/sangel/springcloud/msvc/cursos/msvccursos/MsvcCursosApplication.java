91c1dc2e403a9055464a163672b90a60e57086c5
Samuel Angel Perez <sangelp@unal.edu.co>
2022-07-06 12:11:45 -0500

    add msvc-cursos & modify Maven dependencies

---
+++ kubernetes/msvc-cursos/src/main/java/org/sangel/springcloud/msvc/cursos/msvccursos/MsvcCursosApplication.java
@@ -0,0 +1,13 @@
+package org.sangel.springcloud.msvc.cursos.msvccursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcCursosApplication.class, args);
+	}
+
+}
---
+++ kubernetes/msvc-cursos/src/test/java/org/sangel/springcloud/msvc/cursos/msvccursos/MsvcCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sangel.springcloud.msvc.cursos.msvccursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
