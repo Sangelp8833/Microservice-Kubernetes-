8c48946481a0eb7fa7bba9b73aa19da42d3822f4
Samuel Angel Perez <sangelp@unal.edu.co>
2022-08-19 09:58:54 -0500

    Modificaiciones msvc-usuarios

--- kubernetes/msvc-usuarios/src/main/java/org/sangel/springcloud/msvc/usuarios/msvcusuarios/client/CursoClienteRest.java
+++ kubernetes/msvc-usuarios/src/main/java/org/sangel/springcloud/msvc/usuarios/msvcusuarios/client/CursoClienteRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-cursos", url = "msvc-cursos:8002/curso")
+@FeignClient(name = "msvc-cursos", url = "${msvc.cursos.url}")
 public interface CursoClienteRest {
 
     @DeleteMapping("/borrar-usuario/{usuarioId}")
